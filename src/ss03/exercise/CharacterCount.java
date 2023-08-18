fcf54a667e0224171cc6f808c1066c06da2bcb93
thientran0801 <thientranbk98@gmail.com>
2023-08-18 09:24:34 +0700

    done ss03

---
+++ src/ss03/exercise/CharacterCount.java
@@ -0,0 +1,22 @@
+package ss03.exercise;
+
+import java.util.Scanner;
+
+public class CharacterCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String string = scanner.nextLine();
+        System.out.println(string);
+        System.out.println("Enter the character do you want to count: ");
+        char character = scanner.next().charAt(0);
+        int count = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == character){
+                count++;
+            }
+        }
+        System.out.println("In the string you just entered there arre "+count+" '"+character+"' character.");
+    }
+}
