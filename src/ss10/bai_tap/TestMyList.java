ecd5bc785ddeba7aeef06da0011b2b2fe0b015a2
thientran0801 <thientranbk98@gmail.com>
2023-09-05 00:37:46 +0700

    done

--- src/ss10/bai_tap/TestMyList.java
+++ src/ss10/bai_tap/TestMyList.java
@@ -4,11 +4,30 @@ import java.util.ArrayList;
 
 public class TestMyList {
     public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList();
+        MyList<Integer> myList = new MyList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
 
-    }
-    @Override
-    public String toString(){
-        return "dsjvh";
+        myList.add(6);
+        myList.add(3, 9);
+        myList.remove(6);
+        myList.remove(2);
+
+        System.out.println("Element 0: " + myList.get(0));
+        System.out.println("Element 1: " + myList.get(1));
+        System.out.println("Element 2: " + myList.get(2));
+        System.out.println("Element 3: " + myList.get(3));
+        System.out.println("Element 4: " + myList.get(4));
+//        System.out.println("Element 5: " + myList.get(5));
+//        System.out.println("Element 6: " + myList.get(6));
+//        System.out.println("Element 7: " + myList.get(7));
+
+        System.out.println("Size: " + myList.size());
+        System.out.println("List contains element 3 : " + myList.contains(3) );
+        myList.clear();
+        System.out.println(myList.size());
     }
 }
